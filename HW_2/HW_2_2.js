6b3011e45ed88cca8e03a57e1eb29231d4741c87
SunnyLynx <SunnyKitten-studio@yandex.ru>
2022-04-19 20:09:08 +0500

    first commit

---
+++ HW_2/HW_2_2.js
@@ -0,0 +1,21 @@
+// ****
+let result = prompt('Введите ваш возраст', 'число')
+
+let age = result
+let age_2 = 18
+let age_3 = 60
+
+    if (typeof age != "number") {
+        age = Number(age)
+    }
+    if (age < age_2) {
+        alert("You don’t have access cause your age is " + age + ". It’s less then " + age_2 + ".")
+    } else if (age >= age_2 && age < age_3) {
+        alert("Welcome!")
+    } else if (age >= age_3) {
+        alert("Keep calm and look Culture channel.")
+    } else {
+        alert("Technical work")
+    }
+
+alert
