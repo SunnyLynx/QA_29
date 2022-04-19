308e9730deb904774dc0dff2e889f6b7b3250ded
SunnyLynx <SunnyKitten-studio@yandex.ru>
2022-04-19 21:00:54 +0500

    homework2

--- HW_2/HW_2_1.js
+++ HW_2/HW_2_1.js
@@ -17,11 +17,11 @@ item_5 = item_3
 let item_6
 let item_6_type
 item_6 = 15
-item_6_type = 'number'
+item_6_type = typeof (item_6)
 console.log('item_6 == ' + item_6, '; item_6_type == ' + item_6_type)
 let item_7 = String(item_6)
 let item_7_type
-item_7_type = 'String'
+item_7_type = typeof (item_7)
 console.log('item_7 == ' + item_7,  '; item_7_type == ' + item_7_type)
 let age_1 = 10
 let age_2 = 18
